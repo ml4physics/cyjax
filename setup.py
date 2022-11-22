4ca4b810590e58536d4d45acf361a042aa994040
Mathis Gerdes <MathisGerdes@gmail.com>
2022-11-22 14:56:54 +0100

    Add readthedocs config.

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python
 from setuptools import setup
 
-setup()
+
+setup(
+    name='cyjax',
+    version='1.0.0',
+    description='Numerical methods for Calabi-Yau metrics in JAX',
+    author='Mathis Gerdes',
+    author_email='MathisGerdes@gmail.com',
+    packages=['cyjax'],
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy',
+        'sympy',
+        'jax',
+        'jaxlib',
+        'chex',
+        'progeval',
+        'jax-autovmap',
+        'matplotlib',
+        'flax',
+        'optax',
+    ],
+)
